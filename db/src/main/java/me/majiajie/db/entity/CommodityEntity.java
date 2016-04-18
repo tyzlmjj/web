80878ebeae67737db3a931e99d7a214c544e1fcf
M家杰 <tyzl931019@gmail.com>
2016-04-18 21:18:26 +0800

    update

---
+++ db/src/main/java/me/majiajie/db/entity/CommodityEntity.java
@@ -0,0 +1,78 @@
+package me.majiajie.db.entity;
+
+import javax.persistence.*;
+
+/**
+ * Created by MJJ on 2016/4/18.
+ */
+@Entity
+@Table(name = "commodity", schema = "htest", catalog = "")
+public class CommodityEntity {
+    private int id;
+    private String name;
+    private Double price;
+    private Integer userid;
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name = "id")
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Basic
+    @Column(name = "name")
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Basic
+    @Column(name = "price")
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        CommodityEntity that = (CommodityEntity) o;
+
+        if (id != that.id) return false;
+        if (name != null ? !name.equals(that.name) : that.name != null) return false;
+        if (price != null ? !price.equals(that.price) : that.price != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (price != null ? price.hashCode() : 0);
+        return result;
+    }
+
+    @Basic
+    @Column(name = "userid")
+    public Integer getUserid() {
+        return userid;
+    }
+
+    public void setUserid(Integer userid) {
+        this.userid = userid;
+    }
+}
